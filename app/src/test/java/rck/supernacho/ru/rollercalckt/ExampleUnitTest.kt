eb72c7493f1b013abe32823f09cf8e71690b3aef
SuperNacho <vanbellum@gmail.com>
2018-02-15 12:09:18 +0300

    added test for calc class

--- app/src/test/java/rck/supernacho/ru/rollercalckt/ExampleUnitTest.kt
+++ app/src/test/java/rck/supernacho/ru/rollercalckt/ExampleUnitTest.kt
@@ -3,6 +3,7 @@ package rck.supernacho.ru.rollercalckt
 import org.junit.Test
 
 import org.junit.Assert.*
+import rck.supernacho.ru.rollercalckt.model.Calculator
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +11,18 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+    val calc = Calculator()
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun calc_isCorrect(){
+        assertEquals(50,calc.getLength(144,84,0.212).toInt())
     }
+
+    @Test
+    fun calc_thickness_isCorrect(){
+        assertEquals(0.2, calc.getThickness(5, 1), 0.09)
+        assertEquals(0.5, calc.getThickness(20, 10), 0.09)
+    }
+
+
 }
