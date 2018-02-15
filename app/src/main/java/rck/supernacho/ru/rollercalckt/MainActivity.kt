853ac2f81c11d66ac1c1467ce237e1470b9e232b
SuperNacho <vanbellum@gmail.com>
2018-02-15 18:19:42 +0300

    added calc fragment in main activity

--- app/src/main/java/rck/supernacho/ru/rollercalckt/MainActivity.kt
+++ app/src/main/java/rck/supernacho/ru/rollercalckt/MainActivity.kt
@@ -1,24 +1,26 @@
 package rck.supernacho.ru.rollercalckt
 
+import android.net.Uri
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import rck.supernacho.ru.rollercalckt.fragments.CalcFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CalcFragment.OnFragmentInteractionListener {
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
-                message.setText(R.string.title_home)
+//                message.setText(R.string.title_home)
                 return@OnNavigationItemSelectedListener true
             }
-            R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
+            R.id.navigation_help -> {
+//                message.setText(R.string.title_help)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
+//                message.setText(R.string.title_settings)
                 return@OnNavigationItemSelectedListener true
             }
         }
@@ -28,7 +30,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        init()
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+
+    fun init(){
+        val calcFragment = CalcFragment.newInstance("tt","tt")
+        supportFragmentManager.beginTransaction()
+                .add(R.id.fragment_container, calcFragment)
+                .commit()
+    }
+
+    override fun onFragmentInteraction(uri: Uri) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
