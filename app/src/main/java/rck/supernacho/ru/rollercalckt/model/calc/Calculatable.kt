79beb92add84212587854bea127c1621044ccca3
SuperNacho <vanbellum@gmail.com>
2018-02-15 18:18:31 +0300

    added calc interface

--- app/src/main/java/rck/supernacho/ru/rollercalckt/model/calc/Calculatable.kt
+++ app/src/main/java/rck/supernacho/ru/rollercalckt/model/calc/Calculatable.kt
@@ -4,6 +4,6 @@ package rck.supernacho.ru.rollercalckt.model.calc
  * Created by SuperNacho on 15.02.2018.
  */
 interface Calculatable {
-    fun getLength(dMax: Int, dMin: Int, thick: Double): Double
+    fun getLength(dMax: Int, dMin: Int, thick: Double): String
     fun getThickness(nLayers: Int, rule: Thickness): Double
 }
