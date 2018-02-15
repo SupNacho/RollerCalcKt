7d6f232b48247a9ad2c491de7cc2bec36c1417d3
SuperNacho <vanbellum@gmail.com>
2018-02-15 18:17:34 +0300

    added calc controller that control all calculation work

--- app/src/main/java/rck/supernacho/ru/rollercalckt/controller/CalcController.kt
+++ app/src/main/java/rck/supernacho/ru/rollercalckt/controller/CalcController.kt
@@ -1,7 +1,40 @@
 package rck.supernacho.ru.rollercalckt.controller
 
-/**
- * Created by SuperNacho on 15.02.2018.
- */
-class CalcController {
+import android.content.Context
+import android.view.View
+import android.widget.*
+import kotlinx.android.synthetic.main.fragment_calc.view.*
+import rck.supernacho.ru.rollercalckt.R
+import rck.supernacho.ru.rollercalckt.model.calc.Calculator
+
+class CalcController(context: Context, vararg views: View) : Controllable {
+    val views = views
+    val calculator = Calculator()
+    var out: Int = 0
+    var inn: Int = 0
+    var thickness: Double = 0.215
+    val context = context
+
+    override fun setOuterD(outD: Int) {
+        out = outD
+    }
+
+    override fun setInnerD(innD: Int) {
+        inn = innD
+    }
+
+    override fun setThick(thick: Double) {
+        thickness = thick
+    }
+
+    override fun getLength() {
+        for (view in views) {
+            if (view is EditText && view.id == R.id.calc_fragment_inner_d) inn = view.text.toString().toInt()
+            if (view is EditText && view.id == R.id.calc_fragment_outer_d) out = view.text.toString().toInt()
+        }
+        var result = calculator.getLength(out, inn, thickness)
+        for (view in views){
+            if (view is TextView && view.id == R.id.calc_fragment_text_view_output) view.text = result
+        }
+    }
 }
