7babe32aaf1410c0532fb41b81f3b1546ef8a9b5
SuperNacho <vanbellum@gmail.com>
2018-02-17 18:13:16 +0300

    tweaks for DBHelper

--- app/src/main/java/rck/supernacho/ru/rollercalckt/model/database/DataBaseHelper.kt
+++ app/src/main/java/rck/supernacho/ru/rollercalckt/model/database/DataBaseHelper.kt
@@ -5,52 +5,38 @@ import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
 
-class DataBaseHelper : SQLiteOpenHelper {
-    constructor(context: Context) : super(
-            context,
-            DATABASE_NAME,
-            null,
-            DATABASE_VERSION
-    )
-
-    companion object {
-        private val DATABASE_NAME = "rc_materials.db"
-        private val DATABASE_VERSION = 0
-        private val TABLE_MATERIALS = "brands"
-        private val TABLE_THICKS = "thicks"
-        private val TABLE_RESULTS = "results"
-        private val COLUMN_ID = "_id"
-        private val COLUMN_NAME = "name"
-        private val COLUMN_THICK = "thick"
-        private val COLUMN_ID_BRANDS = "id_brands"
-        private val COLUMN_ID_THICK = "id_thick"
-    }
+class DataBaseHelper(context: Context) : SQLiteOpenHelper(
+        context,
+        DataBaseFields.DATABASE_NAME.field,
+        null,
+        DataBaseFields.DATABASE_VERSION.field.toInt()
+) {
 
 
     override fun onCreate(db: SQLiteDatabase?) {
-        db?.execSQL("CREATE TABLE" + TABLE_MATERIALS + "(\n" +
-                COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT\n" +
+        db?.execSQL("CREATE TABLE" + DataBaseFields.TABLE_MATERIALS.field + "(\n" +
+                DataBaseFields.COLUMN_ID.field + " INTEGER PRIMARY KEY AUTOINCREMENT\n" +
                 "                 NOT NULL,\n" +
-                COLUMN_NAME + " TEXT\n" +
+                DataBaseFields.COLUMN_NAME.field + " TEXT\n" +
                 ");")
-        db?.execSQL("CREATE TABLE " + TABLE_THICKS + " (\n" +
-                COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT\n" +
+        db?.execSQL("CREATE TABLE " +  DataBaseFields.TABLE_THICKS.field + " (\n" +
+                DataBaseFields.COLUMN_ID.field + " INTEGER PRIMARY KEY AUTOINCREMENT\n" +
                 "                  NOT NULL,\n" +
-                COLUMN_THICK + " DOUBLE  NOT NULL\n" +
+                DataBaseFields.COLUMN_THICK.field + " DOUBLE  NOT NULL\n" +
                 ");")
-        db?.execSQL("CREATE TABLE " + TABLE_RESULTS + " (\n" +
-                COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,\n" +
-                COLUMN_ID_BRANDS + " INTEGER REFERENCES brands (_id) ON DELETE CASCADE\n" +
+        db?.execSQL("CREATE TABLE " +  DataBaseFields.TABLE_RESULTS.field + " (\n" +
+                DataBaseFields.COLUMN_ID.field + " INTEGER PRIMARY KEY AUTOINCREMENT,\n" +
+                DataBaseFields.COLUMN_ID_BRANDS.field + " INTEGER REFERENCES brands (_id) ON DELETE CASCADE\n" +
                 "                                              ON UPDATE CASCADE\n" +
                 "                      NOT NULL,\n" +
-                COLUMN_ID_THICK + " INTEGER REFERENCES thicks (_id) ON DELETE CASCADE\n" +
+                DataBaseFields.COLUMN_ID_THICK.field + " INTEGER REFERENCES thicks (_id) ON DELETE CASCADE\n" +
                 "                                              ON UPDATE CASCADE\n" +
                 ");")
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
         if (oldVersion < newVersion) {
-            db?.execSQL("DROP TABLE IF EXISTS " + TABLE_MATERIALS)
+            db?.execSQL("DROP TABLE IF EXISTS " +  DataBaseFields.TABLE_MATERIALS.field)
             onCreate(db)
         }
     }
