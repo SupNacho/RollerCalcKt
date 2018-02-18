60cf73578f4d67d4ce45fcf30b342b0e67cf0251
SuperNacho <vanbellum@gmail.com>
2018-02-18 19:28:44 +0300

    added implementation of ManageableMaterials interface

--- app/src/main/java/rck/supernacho/ru/rollercalckt/controller/CrudMaterialController.kt
+++ app/src/main/java/rck/supernacho/ru/rollercalckt/controller/CrudMaterialController.kt
@@ -1,7 +1,41 @@
 package rck.supernacho.ru.rollercalckt.controller
 
-/**
- * Created by SuperNacho on 18.02.2018.
- */
-class CrudMaterialController {
+import android.content.Context
+import android.view.View
+import android.widget.EditText
+import android.widget.ListView
+import rck.supernacho.ru.rollercalckt.R
+import rck.supernacho.ru.rollercalckt.model.Material
+import rck.supernacho.ru.rollercalckt.model.MaterialMapper
+
+class CrudMaterialController : ManageableMaterials{
+    private val viewsArr : Array<out View>
+    private val materialMapper : MaterialMapper
+    private lateinit var editTextName : EditText
+    private lateinit var editTextThick : EditText
+    private lateinit var listView : ListView
+
+    constructor(context: Context, vararg views: View){
+        viewsArr = views
+        materialMapper = MaterialMapper(context)
+        materialMapper.open()
+        for (view in views){
+            if (view is EditText && view.id == R.id.add_frag_edit_text_material_name) editTextName = view
+            if (view is EditText && view.id == R.id.add_frag_edit_text_material_thick) editTextThick = view
+            if (view is ListView && view.id == R.id.add_frag_list_view_materials) listView = view
+        }
+    }
+
+    override fun add() {
+        materialMapper.insert(editTextName.text.toString(), editTextThick.text.toString().toDouble())
+    }
+
+    override fun remove() {
+        materialMapper.delete(listView.selectedItem as Material)
+    }
+
+    override fun edit() {
+        materialMapper.update(listView.selectedItem as Material, editTextName.text.toString(),
+                editTextThick.text.toString().toDouble())
+    }
 }
