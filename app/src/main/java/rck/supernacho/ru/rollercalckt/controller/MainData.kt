29cb36fde4c3e1197f50eb118af50b015566694b
SuperNacho <vanbellum@gmail.com>
2018-02-20 12:06:10 +0300

    inflate MainData with function

--- app/src/main/java/rck/supernacho/ru/rollercalckt/controller/MainData.kt
+++ app/src/main/java/rck/supernacho/ru/rollercalckt/controller/MainData.kt
@@ -1,7 +1,39 @@
 package rck.supernacho.ru.rollercalckt.controller
 
-/**
- * Created by SuperNacho on 19.02.2018.
- */
+import rck.supernacho.ru.rollercalckt.model.Material
+import rck.supernacho.ru.rollercalckt.model.MaterialMapper
+
 object MainData {
+
+    fun setMaterialMapper(mapMat: MaterialMapper){
+        materialMaper = mapMat
+    }
+
+    fun setMaterialController(matController: ManageableMaterials){
+        materialController = matController
+    }
+    fun setCalcController(calController: Controllable){
+        calcController = calController
+    }
+    fun getMaterialMapper(): MaterialMapper{
+        return materialMaper
+    }
+    fun getMaterialList(): ArrayList<Material>{
+        materials.clear()
+        materials.addAll(materialMaper.getMaterials())
+        return materials
+    }
+
+    fun onResume(){
+        materialMaper.open()
+    }
+
+    fun onDestroy(){
+        materialMaper.close()
+    }
+
+    private lateinit var calcController: Controllable
+    private lateinit var materialController: ManageableMaterials
+    private lateinit var materialMaper: MaterialMapper
+    private val materials: ArrayList<Material> = ArrayList()
 }
