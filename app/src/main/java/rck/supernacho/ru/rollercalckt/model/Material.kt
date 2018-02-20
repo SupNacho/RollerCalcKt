eed44efbd4aa62d1e30169900839e6acc07cc620
SuperNacho <vanbellum@gmail.com>
2018-02-20 12:05:17 +0300

    toString override

--- app/src/main/java/rck/supernacho/ru/rollercalckt/model/Material.kt
+++ app/src/main/java/rck/supernacho/ru/rollercalckt/model/Material.kt
@@ -1,4 +1,7 @@
 package rck.supernacho.ru.rollercalckt.model
 
 class Material(val id: Long, val brand: String, val thickness: Double) {
+    override fun toString(): String {
+        return "${brand} :-> ${thickness}"
+    }
 }
