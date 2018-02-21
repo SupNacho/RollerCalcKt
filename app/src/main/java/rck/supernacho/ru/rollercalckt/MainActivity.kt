587f3ac3dd8e48e79cade681f1aec2a8c2672b5c
SuperNacho <vanbellum@gmail.com>
2018-02-21 16:40:58 +0300

    Implemented bottom navigation calc button

--- app/src/main/java/rck/supernacho/ru/rollercalckt/MainActivity.kt
+++ app/src/main/java/rck/supernacho/ru/rollercalckt/MainActivity.kt
@@ -1,22 +1,56 @@
 package rck.supernacho.ru.rollercalckt
 
+import android.app.FragmentManager
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import rck.supernacho.ru.rollercalckt.controller.MainController
-import rck.supernacho.ru.rollercalckt.fragments.AddMaterialFragment
+import rck.supernacho.ru.rollercalckt.fragments.EditMaterialFragment
 import rck.supernacho.ru.rollercalckt.fragments.CalcFragment
+import rck.supernacho.ru.rollercalckt.fragments.FragmentsTags
 import rck.supernacho.ru.rollercalckt.model.MaterialMapper
 
 class MainActivity : AppCompatActivity(), CalcFragment.OnFragmentInteractionListener,
-        AddMaterialFragment.OnFragmentInteractionListener {
+        EditMaterialFragment.OnFragmentInteractionListener {
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
-//                message.setText(R.string.title_home)
+                Log.d("++","BackStack: ${supportFragmentManager.backStackEntryCount}")
+                if (supportFragmentManager.backStackEntryCount > 0) {
+                    val tag = supportFragmentManager.getBackStackEntryAt(0).name
+                    when(tag) {
+                        FragmentsTags.EDIT_MATERIALS.tag -> {
+                            Log.d("++", "BackStack: ${tag}")
+                            val fragment = supportFragmentManager.findFragmentByTag(FragmentsTags.EDIT_MATERIALS.tag)
+
+                            supportFragmentManager.beginTransaction()
+                                    .remove(fragment)
+                                    .commit()
+                            supportFragmentManager.popBackStack(tag, FragmentManager.POP_BACK_STACK_INCLUSIVE)
+                        }
+                        FragmentsTags.ABOUT.tag -> {
+                            val fragment = supportFragmentManager.findFragmentByTag(FragmentsTags.ABOUT.tag)
+                            supportFragmentManager.beginTransaction()
+                                    .remove(fragment)
+                                    .commit()
+                            supportFragmentManager.popBackStack(tag, FragmentManager.POP_BACK_STACK_INCLUSIVE)
+                        }
+                        FragmentsTags.SETTINGS.tag -> {
+                            val fragment = supportFragmentManager.findFragmentByTag(FragmentsTags.SETTINGS.tag)
+                            supportFragmentManager.beginTransaction()
+                                    .remove(fragment)
+                                    .commit()
+                            supportFragmentManager.popBackStack(tag, FragmentManager.POP_BACK_STACK_INCLUSIVE)
+                        }
+                        else -> {
+                            Toast.makeText(this, "No such fragment", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_help -> {
@@ -39,12 +73,13 @@ class MainActivity : AppCompatActivity(), CalcFragment.OnFragmentInteractionList
         MainController.onCreate()
         init()
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        navigation.selectedItemId = R.id.navigation_home
     }
 
-    fun init(){
+    private fun init(){
         val calcFragment = CalcFragment.newInstance("tt","tt")
         supportFragmentManager.beginTransaction()
-                .add(R.id.fragment_container, calcFragment)
+                .add(R.id.fragment_container, calcFragment, FragmentsTags.CALCULATE.tag)
                 .commit()
     }
 
@@ -52,10 +87,10 @@ class MainActivity : AppCompatActivity(), CalcFragment.OnFragmentInteractionList
         when(command){
             "add_fragment" -> {
                 Toast.makeText(this,"Button Add", Toast.LENGTH_SHORT).show()
-                val addMaterialFragment = AddMaterialFragment.newInstance("tt","tt")
+                val editMaterialFragment = EditMaterialFragment.newInstance("tt","tt")
                 supportFragmentManager.beginTransaction()
-                        .addToBackStack(null)
-                        .add(R.id.fragment_container, addMaterialFragment)
+                        .addToBackStack(FragmentsTags.EDIT_MATERIALS.tag)
+                        .add(R.id.fragment_container, editMaterialFragment, FragmentsTags.EDIT_MATERIALS.tag)
                         .commit()
             }
             else -> {
