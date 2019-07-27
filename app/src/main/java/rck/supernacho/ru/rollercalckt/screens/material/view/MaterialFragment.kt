e4bda8309130732877ce88f77ce155c8fa7360c8
Artem Shumilov <vanbellum@gmail.com>
2019-07-27 08:16:28 +0300

    #6 added debug btn to view old calculator and manage materials in old db

--- app/src/main/java/rck/supernacho/ru/rollercalckt/screens/material/view/MaterialFragment.kt
+++ app/src/main/java/rck/supernacho/ru/rollercalckt/screens/material/view/MaterialFragment.kt
@@ -1,5 +1,6 @@
 package rck.supernacho.ru.rollercalckt.screens.material.view
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,9 +8,11 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
+import kotlinx.android.synthetic.main.fragment_calculation.*
 import org.kodein.di.Kodein
 import org.kodein.di.KodeinAware
 import org.kodein.di.android.x.closestKodein
+import rck.supernacho.ru.rollercalckt.MainActivity
 import rck.supernacho.ru.rollercalckt.R
 import rck.supernacho.ru.rollercalckt.databinding.FragmentMaterialBinding
 import rck.supernacho.ru.rollercalckt.screens.utils.RCViewModelFactory
@@ -28,5 +31,10 @@ class MaterialFragment : Fragment(), KodeinAware {
         val binding: FragmentMaterialBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_material, container, false)
         binding.viewModel = viewModel
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btn_oldCalc.setOnClickListener { startActivity(Intent(context, MainActivity::class.java)) }
     }
 }
