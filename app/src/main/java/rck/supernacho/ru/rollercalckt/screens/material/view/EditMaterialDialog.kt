73cd01c02e106fdb319c4e5359bb8d43ed6cfeb6
Artem Shumilov <vanbellum@gmail.com>
2019-07-28 21:56:31 +0300

    #6 added dialog fragment
    # Conflicts:
    #	app/src/main/java/rck/supernacho/ru/rollercalckt/screens/material/view/EditMaterialDialog.kt
    #	app/src/main/res/layout/edit_material_dialog.xml

--- app/src/main/java/rck/supernacho/ru/rollercalckt/screens/material/view/EditMaterialDialog.kt
+++ app/src/main/java/rck/supernacho/ru/rollercalckt/screens/material/view/EditMaterialDialog.kt
@@ -1,4 +1,17 @@
 package rck.supernacho.ru.rollercalckt.screens.material.view
 
-class EditMaterialDialog {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+
+class EditMaterialDialog: DialogFragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
