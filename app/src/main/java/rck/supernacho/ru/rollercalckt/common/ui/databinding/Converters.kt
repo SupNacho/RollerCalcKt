d39f25eaeadc6f35928cb2098430317b9f9da522
Artem Shumilov <vanbellum@gmail.com>
2019-07-28 10:35:01 +0300

    #6 added swipe menu to materials recycler view

--- app/src/main/java/rck/supernacho/ru/rollercalckt/common/ui/databinding/Converters.kt
+++ app/src/main/java/rck/supernacho/ru/rollercalckt/common/ui/databinding/Converters.kt
@@ -5,3 +5,4 @@ import java.math.BigDecimal
 
 @BindingConversion
 fun bigDecimalToString(value: BigDecimal) = value.toPlainString()
+fun bigDecimalToString(value: BigDecimal?) = value?.toPlainString() ?: BigDecimal.ZERO
