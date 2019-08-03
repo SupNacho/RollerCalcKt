802273d8cfdfe2ec7a7a744c71bfccda802a99c7
Artem Shumilov <vanbellum@gmail.com>
2019-08-03 09:41:10 +0300

    #6 added dialog fragment
    extracted string resource

--- app/src/main/java/rck/supernacho/ru/rollercalckt/screens/material/view/EditMaterialDialog.kt
+++ app/src/main/java/rck/supernacho/ru/rollercalckt/screens/material/view/EditMaterialDialog.kt
@@ -5,13 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
+import rck.supernacho.ru.rollercalckt.R
 
 class EditMaterialDialog: DialogFragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+        return inflater.inflate(R.layout.edit_material_dialog, container, false)
     }
 }
