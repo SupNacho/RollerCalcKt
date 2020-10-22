7ebe6755e25011c43981c4a324414999cc85faae
Artem Shumilov <vanbellum@gmail.com>
2020-10-22 13:32:36 +0300

    fixed bottom nav bar on edit material screen

--- app/src/main/java/rck/supernacho/ru/rollercalckt/NavActivity.kt
+++ app/src/main/java/rck/supernacho/ru/rollercalckt/NavActivity.kt
@@ -2,6 +2,10 @@ package rck.supernacho.ru.rollercalckt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.NavigationUI.setupWithNavController
 import kotlinx.android.synthetic.main.activity_nav.*
@@ -12,5 +16,14 @@ class NavActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nav)
         setupWithNavController(bnv_Navigation, navHostFragment.findNavController())
+        navHostFragment.findNavController().addOnDestinationChangedListener(object : NavController.OnDestinationChangedListener{
+            override fun onDestinationChanged(controller: NavController, destination: NavDestination, arguments: Bundle?) {
+                when(destination.id) {
+                    R.id.manageMaterial -> bnv_Navigation.visibility = View.GONE
+                    else -> bnv_Navigation.visibility = View.VISIBLE
+
+                }
+            }
+        })
     }
 }
