9867dfdad68043d89581b31c154fdc80bb57870e
Artem Shumilov <vanbellum@gmail.com>
2020-10-24 17:38:47 +0300

    added sort icon action colorize

--- app/src/main/java/rck/supernacho/ru/rollercalckt/screens/custom/SortView.kt
+++ app/src/main/java/rck/supernacho/ru/rollercalckt/screens/custom/SortView.kt
@@ -2,6 +2,7 @@ package rck.supernacho.ru.rollercalckt.screens.custom
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.text.style.LineHeightSpan
 import android.util.AttributeSet
 import android.widget.FrameLayout
 import kotlinx.android.synthetic.main.input_view.view.*
@@ -67,18 +68,73 @@ class SortView : FrameLayout {
     }
 
     fun setNameSortAction(action: () -> Unit) {
-        iv_nameSort.setOnClickListener { action() }
+        iv_nameSort.setOnClickListener {
+            setActiveSort(isName = true)
+            action()
+        }
     }
 
     fun setThickSortAction(action: () -> Unit) {
-        iv_thickSort.setOnClickListener { action() }
+        iv_thickSort.setOnClickListener {
+            setActiveSort(isThick = true)
+            action()
+        }
     }
 
     fun setWeightSortAction(action: () -> Unit) {
-        iv_weightSort.setOnClickListener { action() }
+        iv_weightSort.setOnClickListener {
+            setActiveSort(isWeight = true)
+            action()
+        }
     }
 
     fun setDensitySortAction(action: () -> Unit) {
-        iv_densitySort.setOnClickListener { action() }
+
+        iv_densitySort.setOnClickListener {
+            setActiveSort(isDensity = true)
+            action()
+        }
+    }
+
+    private fun setActiveSort(isName: Boolean = false, isThick: Boolean = false, isWeight: Boolean = false, isDensity: Boolean = false) {
+        val (sortDescription, backGround) = when {
+            isName -> {
+                setIcon(nameIcon = R.drawable.ic_name_active)
+                R.string.sort_by_name to R.drawable.spacer_active_small
+            }
+            isThick -> {
+                setIcon(thickIcon = R.drawable.ic_thick_active)
+                R.string.sort_by_thick to R.drawable.spacer_active_small
+            }
+            isWeight -> {
+                setIcon(weightIcon = R.drawable.ic_weight_active)
+                R.string.sort_by_weight to R.drawable.spacer_active_small
+            }
+            isDensity -> {
+                setIcon(densityIcon = R.drawable.ic_density_active)
+                R.string.sort_by_density to R.drawable.spacer_active_small
+            }
+            else -> {
+                setIcon()
+                R.string.sort_by to R.drawable.spacer_dark_small
+            }
+        }
+
+        tv_sortDescription.setText(sortDescription)
+        v_sortDescription.setBackgroundResource(backGround)
+        v_sortDescriptionStarter.setBackgroundResource(R.drawable.spacer_dark)
+        tv_sortDescription.setAppearance(R.style.Text_Dark_Hint)
+
+    }
+
+    private fun setIcon(
+            nameIcon: Int = R.drawable.ic_name_sort,
+            thickIcon: Int = R.drawable.ic_thick_sort,
+            weightIcon: Int = R.drawable.ic_weight_sort,
+            densityIcon: Int = R.drawable.ic_density_sort) {
+        iv_nameSort.setImageResource(nameIcon)
+        iv_thickSort.setImageResource(thickIcon)
+        iv_weightSort.setImageResource(weightIcon)
+        iv_densitySort.setImageResource(densityIcon)
     }
 }
